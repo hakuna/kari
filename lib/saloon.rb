c48af76dbd096c325bf793d24bc3a0d9d221cbb3
Steven Schmid <stevschmid@gmail.com>
2022-09-07 13:53:01 +0200

    Initial commit

---
+++ lib/saloon.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "saloon/version"
+
+# Saloon
+module Saloon
+end
---
+++ spec/saloon_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Saloon do
+  it "has a version number" do
+    expect(Saloon::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
